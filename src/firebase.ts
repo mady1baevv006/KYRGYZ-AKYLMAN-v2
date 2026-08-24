51df851fefb459010f210d2e5aeb5f09d4a9b279
mady1baevv006 <mady1baevv@kyrgyzakylman.com>
2026-08-24 13:43:05 +0600

    Add Firebase authentication setup and functions

---
+++ src/firebase.ts
@@ -0,0 +1,35 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBEZQvst6Scv5ZHqwoYrsPuqyJ24iNEzo",
+  authDomain: "kyrgyz-akylman.firebaseapp.com",
+  projectId: "kyrgyz-akylman",
+  storageBucket: "kyrgyz-akylman.firebasestorage.app",
+  messagingSenderId: "927724870331",
+  appId: "1:927724870331:web:c5fb065eb20f389cfc279e",
+  measurementId: "G-93DBMQLLD6"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+// Функция для входа
+export const loginWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    return result.user;
+  } catch (error) {
+    console.error("Ошибка авторизации:", error);
+  }
+};
+
+// Функция для выхода
+export const logout = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error("Ошибка выхода:", error);
+  }
+};
