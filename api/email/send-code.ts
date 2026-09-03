160079a137161343dac523a4efeac56714aa4d21
mady1baevv006 <mady1baevv@kyrgyzakylman.com>
2026-09-03 17:52:46 +0600

    Update send-code.ts

--- api/email/send-code.ts
+++ api/email/send-code.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Resend } from 'resend';
-import { getFirestoreAdmin } from '../firebaseAdmin';
+import { getFirestoreAdmin } from '../firebaseAdmin.js';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
