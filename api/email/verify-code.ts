d0922045dfc0ad1e42bbe966f1194ef7593ddead
mady1baevv006 <mady1baevv@kyrgyzakylman.com>
2026-09-03 17:53:03 +0600

    Update verify-code.ts

--- api/email/verify-code.ts
+++ api/email/verify-code.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getFirestoreAdmin } from '../firebaseAdmin';
+import { getFirestoreAdmin } from '../firebaseAdmin.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
